8dc280f6780a060271c12c1c60ad96a812e61b64
starlord2015 <noreen.ward@rocketmail.com>
2015-02-20 12:32:44 +0000

    This is a side project to familiarise myself with asp.net mvc web applications

---
+++ MvcTest/MvcTest/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Hello Marta!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "I'm customising the code...";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "...to make it look prettier";
+
+            return View();
+        }
+    }
+}
---
+++ MvcTest/MvcTest/Controllers/MapsControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using MvcTest;
+using MvcTest.Controllers;
+
+namespace MvcBasicWalkthrough.Tests.Controllers
+{
+    [TestClass]
+    public class MapsControllerTest
+    {
+        [TestMethod]
+        public void ViewMaps()
+        {
+            // Arrange
+            MapsController controller = new MapsController();
+
+            // Act
+            ViewResult result = controller.ViewMaps() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
